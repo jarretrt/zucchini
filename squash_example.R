03b39e61f58d88bcf53d5a577fed1ecc2106246c
Matt Shotwell <biostatmatt@gmail.com>
2020-05-19 20:53:34 -0500

    updated example graphics

--- squash_example.R
+++ squash_example.R
@@ -1,41 +1,51 @@
 library('dplyr')
+library('ggplot2')
+library('magrittr')
+
 source('squash.R')
+
 data('iris')
 
 ## squash iris data
 iris_squash <- squash(iris)
 
 ## plot original and squashed data
-with(dat, plot(Sepal.Width, Sepal.Length, pch=20, col='gray'))
-with(dat_m, points(Sepal.Width, Sepal.Length, pch=20, col='#800080'))
+iris_all <- bind_rows(
+  "Original"=iris,
+  "Squashed"=iris_squash,
+  .id='Dataset')
+
+iris_ggp <- iris_all %>%
+  ggplot(aes(x=Sepal.Width, y=Sepal.Length, col=Species)) +
+  facet_wrap(~Dataset) +
+  geom_point() + 
+  theme(legend.position = 'top')
+print(iris_ggp)
 
 ## data for prediction
+rng <- range(iris$Sepal.Width)
 pre <- expand.grid(
-  Sepal.Width = seq(min(dat$Sepal.Width),
-    max(dat$Sepal.Width), length.out=100),
-  Species = unique(dat$Species))
+  Sepal.Width = seq(rng[1], rng[2], length.out=100),
+  Species = unique(iris$Species))
 
-## compare linear model fits using lm
-fit_n <- lm(Sepal.Length ~ Sepal.Width * Species, data=dat)
-pre_n <- predict(fit_n, newdata=pre, interval='confidence')
-cbind(pre, pre_n) %>%
-  group_by(Species) %>%
-  group_walk(function(x, y) {
-    lines(x$Sepal.Width, x$fit, col='gray', lwd=2)
-    polygon(x=c(x$Sepal.Width, rev(x$Sepal.Width)),
-            y=c(x$lwr, rev(x$upr)), border=NA, col='#00000022')
-  })
+## fit original data using lm
+fit_n <- lm(Sepal.Length ~ Sepal.Width * Species, data=iris)
 fit_n %>% summary
+pre_n <- predict(fit_n, newdata=pre, interval='confidence', )
+pre_n <- cbind(pre, pre_n) %>% mutate(Dataset='Original')
 
+## fit squashed data using lm
 ## 'df.residual' needs to be corrected for this to work
-fit_m <- lm(Sepal.Length ~ Sepal.Width * Species, data=dat_m,
-  weights=dat_m$`(weight)`)
+fit_m <- lm(Sepal.Length ~ Sepal.Width * Species,
+  data=iris_squash, weights=iris_squash$`(weight)`)
 fit_m$df.residual <- with(fit_m, sum(weights)-length(coefficients))
-pre_m <- predict(fit_m, newdata=pre, interval='confidence')
-cbind(pre, pre_m) %>%
-  group_by(Species) %>%
-  group_walk(function(x, y) {
-    lines(x$Sepal.Width, x$fit, col='#800080', lty=2, lwd=2)
-    polygon(x=c(x$Sepal.Width, rev(x$Sepal.Width)),
-            y=c(x$lwr, rev(x$upr)), border=NA, col='#80008022')
-  })
+pre_m <- predict(fit_m, newdata=pre, interval='confidence', )
+pre_m <- cbind(pre, pre_m) %>% mutate(Dataset='Squashed')
+
+## plot model fits for both data sets
+pre <- bind_rows(pre_n, pre_m)
+iris_ggp +
+  geom_line(data=pre, aes(y=fit)) +
+  geom_ribbon(data=pre, aes(y=fit, ymin=lwr, ymax=upr,
+    fill=Species), alpha=0.5) +
+  theme(legend.position = 'top')
